118e17f4097ba8dcb450533c7f9adf5187a4ad2c
hundsun <1055038041@qq.com>
2019-12-20 15:41:16 +0800

    代理模式示例代码， 代理jdbc操作

--- src/main/java/factory/ProxyFactory.java
+++ src/main/java/factory/ProxyFactory.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.ConsoleHandler;
 
 public class ProxyFactory implements InvocationHandler {
 
